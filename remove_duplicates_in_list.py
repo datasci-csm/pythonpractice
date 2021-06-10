f4e337799018791dbf9e31a97db8b671c833ba03
V <vtzuang@gmail.com>
2021-06-09 23:51:35 -0700

    Revised file to create functions to remove duplicates

--- remove_duplicates_in_list.py
+++ remove_duplicates_in_list.py
@@ -3,21 +3,36 @@
 
 # using a loop:
 
-no_duplicates = []
-
 duplicates_list = ["hi", "bye", "sun", "hi", "dog", "cat", "dog", "hot", "cold", "cold"]
 
-for value in duplicates_list:
-    if value in no_duplicates:
-        pass
-    else:
-        no_duplicates.append(value)
 
-print("duplicates_list = ", duplicates_list)
-print("no_duplicates = ", no_duplicates)
+def remove_dup(duplicates_list):
+
+    no_duplicates = []
+    
+    for value in duplicates_list:
+        if value in no_duplicates:
+            pass
+        else:
+            no_duplicates.append(value)
+
+    return no_duplicates
+
+
 
 # now with a set:
 
-set_no_duplicate = set(duplicates_list)
+def remove_dup_set(duplicates_list):
+    
+    set_no_duplicate = set(duplicates_list)
+    return set_no_duplicate
 
-print("set_no_duplicate = " , set_no_duplicate)
+
+
+if __name__ == "__main__":
+    no_duplicates = remove_dup(duplicates_list)
+    print("duplicates_list = ", duplicates_list)
+    print("no_duplicates = ", no_duplicates)
+
+    no_duplicates_set = remove_dup_set(duplicates_list)
+    print("no_duplicate_set = " , no_duplicates_set)
